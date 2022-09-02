403ecde0f8d529b4e8117fbeb0c1a7eb21d2cad2
Daniel Thornburgh <mysterymath@gmail.com>
2022-09-02 14:41:19 -0700

    Run clang-format.

--- 01_Hello/hello.c
+++ 01_Hello/hello.c
@@ -11,39 +11,31 @@
 #define LT_GY 0x10
 #define WHITE 0x30
 
-int main (void) {
-  const char text[]="Hello World!"; // zero terminated c string
-  const char palette[]={
-    BLACK, DK_GY, LT_GY, WHITE,
-    0,0,0,0,
-    0,0,0,0,
-    0,0,0,0
-  };
+int main(void) {
+  const char text[] = "Hello World!"; // zero terminated c string
+  const char palette[] = {BLACK, DK_GY, LT_GY, WHITE, 0, 0, 0, 0,
+                          0,     0,     0,     0,     0, 0, 0, 0};
 
-	ppu_off(); // screen off
+  ppu_off(); // screen off
 
-	pal_bg(palette); //	load the BG palette
+  pal_bg(palette); //	load the BG palette
 
-	// set a starting point on the screen
-	// vram_adr(NTADR_A(x,y));
-	vram_adr(NTADR_A(10,14)); // screen is 32 x 30 tiles
+  // set a starting point on the screen
+  // vram_adr(NTADR_A(x,y));
+  vram_adr(NTADR_A(10, 14)); // screen is 32 x 30 tiles
 
-	for (char i = 0; text[i]; ++i)
-		vram_put(text[i]); // this pushes 1 char to the screen
+  for (char i = 0; text[i]; ++i)
+    vram_put(text[i]); // this pushes 1 char to the screen
 
-	// vram_adr and vram_put only work with screen off
-	// NOTE, you could replace everything between i = 0; and here with...
-	// vram_write(text,sizeof(text));
-	// does the same thing
+  // vram_adr and vram_put only work with screen off
+  // NOTE, you could replace everything between i = 0; and here with...
+  // vram_write(text,sizeof(text));
+  // does the same thing
 
-	ppu_on_all(); //	turn on screen
+  ppu_on_all(); //	turn on screen
 
-
-	while (1){
-		// infinite loop
-		// game code can go here later.
-
-	}
+  while (1) {
+    // infinite loop
+    // game code can go here later.
+  }
 }
-
-
