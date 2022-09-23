223607896c9273398cc4dedadd65486cfb4e1234
Daniel Thornburgh <mysterymath@gmail.com>
2022-09-23 10:15:46 -0700

    Include asm textually.

--- 18_Music/MUSIC/music.s
+++ 18_Music/MUSIC/music.s
@@ -1,7 +1,7 @@
 .section .rodata.music_data,"a",@progbits
 .globl music_data
 music_data:
-  .incbin "TestMusic3.s"
+  .include "TestMusic3.s"
 
 .section .rodata.sounds_data,"a",@progbits
 .globl sounds_data
--- 19_SFX/MUSIC/music.s
+++ 19_SFX/MUSIC/music.s
@@ -1,9 +1,9 @@
 .section .rodata.music_data,"a",@progbits
 .globl music_data
 music_data:
-  .incbin "TestMusic3.s"
+  .include "TestMusic3.s"
 
 .section .rodata.sounds_data,"a",@progbits
 .globl sounds_data
 sounds_data:
-  .incbin "SoundFx.s"
+  .include "SoundFx.s"
